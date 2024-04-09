d3581b34827a8b8313918a5397f10d0483a5c3d0
AfelipeAntunes <a.felipe@outlook.com>
2024-04-09 20:16:54 -0300

    M1S09 - EXERCÍCIO 4

--- keep_health_project/src/app/login/login.component.ts
+++ keep_health_project/src/app/login/login.component.ts
@@ -1,78 +1,46 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
-interface Usuario {
-  email: string;
-  senha: string;
-}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  standalone: true,
-  imports: [FormsModule]
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  email = '';
+  senha = '';
 
-  email: string = "";
-  senha: string = "";
+  constructor(private router: Router) {}
 
-  constructor(private router: Router = Inject(Router)) { }
+  entrar() {
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const usuarioEncontrado = usuarios.find(
+      (usuario: { email: string; senha: string; }) => usuario.email === this.email && usuario.senha === this.senha
+    );
 
-  ngOnInit(): void { }
-
-  entrar(): void {
-    // Valida se o email e a senha estão preenchidos
-    if (!this.email || !this.senha) {
-      return alert("Preencha todos os campos!");
-    }
-
-    // Valida se o email é válido
-    if (!this.validarEmail(this.email)) {
-      return alert("Email inválido!");
-    }
-
-    // Valida se a senha é válida
-    if (!this.validarSenha(this.senha)) {
-      return alert("Senha inválida!");
-    }
-
-    // Simula a autenticação com um objeto localStorage
-    const usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
-
-    if (usuarios) {
-      const usuarioEncontrado = usuarios.find((usuario: Usuario) => usuario.email === this.email && usuario.senha === this.senha);
-
-      if (usuarioEncontrado) {
-        // Redireciona para a página inicial
-        this.router.navigate(["/home"]);
-      } else {
-        alert("Usuário não encontrado!");
-      }
+    if (usuarioEncontrado) {
+      // Redirecionar para a home
+      this.router.navigate(['/home']);
     } else {
-      // Exibir mensagem de erro informando que a lista de usuários está vazia
-      alert("Erro ao carregar a lista de usuários!");
+      alert('Usuário ou senha inválidos');
     }
   }
 
-  esqueciSenha(): void {
-    alert("Sua senha foi alterada para 'a1b2c4d4'.");
+  esqueciSenha() {
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const usuarioEncontrado = usuarios.find((usuario: { email: string; }) => usuario.email === this.email);
+
+    if (usuarioEncontrado) {
+      usuarioEncontrado.senha = 'a1b2c4d4';
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+      alert('Sua senha foi alterada para "a1b2c4d4".');
+    } else {
+      alert('Usuário não encontrado.');
+    }
   }
 
-  cadastrar(): void {
-    // Redireciona para a página de cadastro
-    this.router.navigate(["/cadastro"]);
+  cadastrar() {
+    // Redirecionar para a tela de cadastro
+    this.router.navigate(['/cadastro']);
   }
-
-  validarEmail(email: string): boolean {
-    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
-  }
-
-  validarSenha(senha: string): boolean {
-    return senha.length >= 6;
-  }
-
 }
