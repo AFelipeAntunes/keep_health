dee6808dff04ea780fe2388a4db49334555b252e
AfelipeAntunes <a.felipe@outlook.com>
2024-04-14 20:07:02 -0300

    Etapa 3 - Validação de login

--- Keep_Health_Project/src/app/app.component.ts
+++ Keep_Health_Project/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AppRoutingModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Keep_Health_Project';
