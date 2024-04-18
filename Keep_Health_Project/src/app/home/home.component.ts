c40160eeed219be8fc4c1b2bde9921ce52932fae
AfelipeAntunes <a.felipe@outlook.com>
2024-04-17 23:11:18 -0300

    Etapa 16: Diálogo para Adicionar Atividade

--- Keep_Health_Project/src/app/home/home.component.ts
+++ Keep_Health_Project/src/app/home/home.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+interface Atividade {
+  tipo: string;
+  data: Date;
+  distancia?: number;
+  tempo?: string;
+}
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DialogModule,
+    ButtonModule,
+    DropdownModule,
+    CalendarModule,
+    InputTextModule,
+  ],
 })
 export class HomeComponent {
+  displayDialog: boolean = false;
+  tiposAtividade: any[] = [
+    { label: 'Corrida', value: 'corrida' },
+    { label: 'Caminhada', value: 'caminhada' },
+    { label: 'Natação', value: 'natacao' },
+  ];
+  atividade: Atividade = {
+    tipo: '',
+    data: new Date(),
+  };
 
+  showDialog() {
+    this.displayDialog = true;
+  }
+
+  salvarAtividade() {
+    let atividades: Atividade[] = JSON.parse(localStorage.getItem('atividades') || '[]');
+    atividades.push(this.atividade);
+    localStorage.setItem('atividades', JSON.stringify(atividades));
+    this.atividade = { tipo: '', data: new Date() }; 
+    this.displayDialog = false;
+  }
 }
