9183fa7fd0ff147c9dd7758fd9b548718ac8d5e7
AfelipeAntunes <a.felipe@outlook.com>
2024-04-17 21:38:58 -0300

    Etapa 12: Módulo Compartilhado

--- Keep_Health_Project/src/app/app.component.ts
+++ Keep_Health_Project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent, SidebarComponent } from './shared/shared'; // Importe os componentes compartilhados
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppRoutingModule],
+  imports: [AppRoutingModule, HeaderComponent, SidebarComponent], // Adicione os componentes compartilhados
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
